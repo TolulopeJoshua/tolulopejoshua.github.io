22f2bbe088daaa304ec5d3728cfc1f3edc42622e
TolulopeJoshua <babtol235@gmail.com>
2022-07-03 17:56:45 +0100

    gh-pages3

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ import {AiOutlineTwitter} from 'react-icons/ai'
 const Footer = () => {
   return (
     <footer>
-      <a href="/" className='footer__logo'>ToluJosh</a>
+      <a href="#" className='footer__logo'>ToluJosh</a>
 
       <ul className="permalinks">
         <li><a href="#">Home</a></li>
--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import CV from '../../assets/resume_mern.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href="/" className='btn'>Download CV</a>
+        <a href={CV} target="_blank" className='btn' rel="noreferrer">Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
