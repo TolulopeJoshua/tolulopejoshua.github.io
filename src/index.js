5a8694b1db034c59af6d7d4bd9f3003ed132bf09
TolulopeJoshua <babtol235@gmail.com>
2022-10-19 09:13:33 +0100

    aos v1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import App from './App'
 import './index.css'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
-Aos.init({offset: 100, duration: 600});
+Aos.init({offset: 150, duration: 600});
 
 const root = ReactDom.createRoot(document.querySelector('#root'))
 root.render(<App/>)
