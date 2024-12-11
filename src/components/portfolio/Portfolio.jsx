698a4984dabddd2ecab64b2c0e138aa411938ee3
e4ewebdev <webdev@e4email.net>
2024-12-11 15:37:30 +0100

    added blog api

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,7 +5,7 @@ import IMG2 from '../../assets/elibrary.jpg'
 import IMG3 from '../../assets/quiz-app.jpg'
 import IMG4 from '../../assets/dealership.jpg'
 import IMG5 from '../../assets/ludo-board.png'
-import IMG6 from '../../assets/planner.jfif'
+import IMG6 from '../../assets/api.jpg'
 
 const Portfolio = () => {
   return (
@@ -38,7 +38,7 @@ const Portfolio = () => {
           <div className="portfolio__item-image">
             <img src={IMG4} alt="dealership" />
           </div>
-          <h3>Car Dealership with NextJS</h3>
+          <h3>Car Dealership (Clone) with NextJS</h3>
           <div className="portfolio__item-cta">
             <a href="https://github.com/TolulopeJoshua/dealer_shop" className='btn' target='_blank' rel="noreferrer">Github</a>
             <a href="https://dealershop.vercel.app//" className="btn btn-primary" target='_blank' rel="noreferrer">Live Demo</a>
@@ -66,12 +66,12 @@ const Portfolio = () => {
         </article> */}
         <article data-aos="flip-down" className="portfolio__item">
           <div className="portfolio__item-image">
-            <img src={IMG6} alt="planner app" />
+            <img src={IMG6} alt="blogging api" />
           </div>
-          <h3>Planner App (Mobile)</h3>
+          <h3>Blogging Rest API (NodeJs)</h3>
           <div className="portfolio__item-cta">
-            <a href="https://github.com/TolulopeJoshua/bible-planner" className='btn' target='_blank' rel="noreferrer">Github</a>
-            <a href="https://play.google.com/store/apps/details?id=com.ToluJosh.biblereadingplanner" className="btn btn-primary" target='_blank' rel="noreferrer">Live Demo</a>
+            <a href="https://github.com/TolulopeJoshua/Blogging-API" className='btn' target='_blank' rel="noreferrer">Github</a>
+            {/* <a href="https://play.google.com/store/apps/details?id=com.ToluJosh.biblereadingplanner" className="btn btn-primary" target='_blank' rel="noreferrer">Live Demo</a> */}
           </div>
         </article>
         <article data-aos="flip-down" className="portfolio__item">
